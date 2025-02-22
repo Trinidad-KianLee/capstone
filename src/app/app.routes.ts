27cb5c9a9c2d1a0453af3b27d71c1b8732c12ab8
Kian Lee Trinidad <trinidadkianleeb@gmail.com>
2025-02-22 22:25:55 +0800

    add task

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { TaskComponent } from './pages/task/task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'tasks', component: TaskComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, 
+  { path: '**', redirectTo: '/dashboard' } 
+];
