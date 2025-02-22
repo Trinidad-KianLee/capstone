a54eade6ee7ca878aae86bcd2f4c8a7dd7c4b3f1
Kian Lee Trinidad <trinidadkianleeb@gmail.com>
2025-02-22 23:17:37 +0800

    update ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TaskComponent } from './pages/task/task.component';
+import { DocumentComponent } from './pages/document/document.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'tasks', component: TaskComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, 
-  { path: '**', redirectTo: '/dashboard' } 
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'task', component: TaskComponent },
+    { path: 'document', component: DocumentComponent},
+    { path: 'activities', component: ActivitiesComponent},
 ];
