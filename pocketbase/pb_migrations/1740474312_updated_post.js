dcbd487307e573c799b734d88696dee2a2cb765d
Kian Lee Trinidad <trinidadkianleeb@gmail.com>
2025-03-10 09:31:26 +0800

    add pocketbase data

---
+++ pocketbase/pb_migrations/1740474312_updated_post.js
@@ -0,0 +1,134 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2106002237")
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2363381545",
+    "max": 0,
+    "min": 0,
+    "name": "type",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2063623452",
+    "max": 0,
+    "min": 0,
+    "name": "status",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(4, new Field({
+    "hidden": false,
+    "id": "date949509141",
+    "max": "",
+    "min": "",
+    "name": "submission_date",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "date"
+  }))
+
+  // add field
+  collection.fields.addAt(5, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3525919832",
+    "max": 0,
+    "min": 0,
+    "name": "feedback",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1204587666",
+    "max": 0,
+    "min": 0,
+    "name": "action",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3065852031",
+    "max": 0,
+    "min": 0,
+    "name": "document",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2106002237")
+
+  // remove field
+  collection.fields.removeById("text2363381545")
+
+  // remove field
+  collection.fields.removeById("text2063623452")
+
+  // remove field
+  collection.fields.removeById("date949509141")
+
+  // remove field
+  collection.fields.removeById("text3525919832")
+
+  // remove field
+  collection.fields.removeById("text1204587666")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3065852031",
+    "max": 0,
+    "min": 0,
+    "name": "message",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})
