49d9577f0681a10c26b309aabe6787e1517979d2
Kian Lee Trinidad <trinidadkianleeb@gmail.com>
2025-03-10 09:29:08 +0800

    update changes regarding tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TaskComponent } from './pages/task/task.component';
 import { DocumentComponent } from './pages/document/document.component';
 import { ActivitiesComponent } from './pages/activities/activities.component';
-import { LoginComponent } from './pages/login/login.component'; // Import the login component
+import { LoginComponent } from './pages/login/login.component'; 
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,4 @@
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui") 
-  ],
-  daisyui: {
-    themes: [
-      {
-        sentinel: {
-          "primary": "#0026FF",
-          "secondary": "#F8F9FC",
-          "accent": "#F4E37C",
-          "neutral": "#6B7280",
-          "base-100": "#FFFFFF",
-          "info": "#3B82F6",
-          "success": "#34D399",
-          "warning": "#FBBF24",
-          "error": "#F87171",
-        },
-      },
-    ],
-  },
 }
