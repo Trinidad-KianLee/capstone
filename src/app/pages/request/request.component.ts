ce233f2a66e6465ef64520b49c84ed5c32251322
Escuro-JohnDenielL <johndenielescuro@gmail.com>
2025-03-10 20:52:40 +0800

    add unfinished changes

--- src/app/pages/request/request.component.ts
+++ src/app/pages/request/request.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-request',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], // <-- Important for *ngIf, ngFor, etc.
   templateUrl: './request.component.html',
   styleUrl: './request.component.css'
 })
 export class RequestComponent {
+  step = 1; // 1 = contract selection, 2 = form
 
+  goNext() {
+    // Only the "Next" button will trigger the form
+    this.step = 2;
+  }
+
+  goBack() {
+    this.step = 1;
+  }
 }
